6bf3b4441293e044087e5918ef1baa87941e8a5e
aramburo reyes francisco eduardo <aramburoeduardo972@gmail.com>
2025-11-22 09:30:16 -0800

    Initial Commit

---
+++ GlanceableFitness Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GlanceableFitness Watch App
+//
+//  Created by francisco eduardo aramburo reyes on 22/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GlanceableFitness Watch App/GlanceableFitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlanceableFitnessApp.swift
+//  GlanceableFitness Watch App
+//
+//  Created by francisco eduardo aramburo reyes on 22/11/25.
+//
+
+import SwiftUI
+
+@main
+struct GlanceableFitness_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
